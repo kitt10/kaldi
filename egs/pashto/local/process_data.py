dc9efc62bdf4abb2a16c8d28cae976c35ee4faf9
kitt10 <bulinmartin@gmail.com>
2018-10-22 13:55:00 -0400

    process_data looks fine now

--- egs/pashto/local/process_data.py
+++ egs/pashto/local/process_data.py
@@ -7,11 +7,12 @@
 # images.scp - matches the image is's with the actual image file
 
 from argparse import ArgumentParser
-from os import listdir, path
-from glob import glob
+from os import listdir
+from os.path import join as join_path
 from shutil import copyfile
 from codecs import open as cod_open
 from random import randint
+from PIL import Image, ImageOps
 
 def parse_args():
     parser = ArgumentParser(description='Creates data/train and data/test.')
@@ -19,6 +20,8 @@ def parse_args():
     parser.add_argument('--data_path_im', type=str, help='path to the PASHTO data images')
     parser.add_argument('--out_dir', type=str, default='data', help='where to create the train and test data directories')
     parser.add_argument('--spks', type=str, help='list of speakers (string with space sep)')
+    parser.add_argument('--n_samples', type=int, help='number of samples per speaker to consider')
+    parser.add_argument('--invert', type=bool, default=False, help='invert colors (black/white) for all images')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -34,34 +37,38 @@ if __name__ == '__main__':
 
     spks_count = dict([(spk, 0) for spk in args.spks.split()])
     for w_id in listdir(args.data_path_im):
-        trs = dict()
-        with cod_open(args.data_path_tr+'/'+w_id+'.txt', 'r', encoding='utf-8') as f:
+        with cod_open(join_path(args.data_path_tr, w_id+'.txt'), 'r', encoding='utf-8') as f:
             for line in f.readlines():
-                trs[line[line.find('(')+1:line.find(')')]] = line[4:line.find(' </s>')]
+                im_orig_id = line[line.find('(')+1:line.find(')')]
+                trs = line[4:line.find(' </s>')]
 
-        for im_path_orig in glob(path.join(args.data_path_im+w_id, '*'+im_mimetype)):
-            im_filename = im_path_orig.split('/')[-1]
-            spk_id = im_filename.split('_')[1]
-            if spk_id not in spks_count.keys():
-                continue
+                im_dir = im_orig_id[im_orig_id.find('-')+1:]
+                im_orig_path = join_path(args.data_path_im, im_dir, im_orig_id+im_mimetype)
+                im_orig_filename = im_orig_path.split('/')[-1]
+                spk_id = im_orig_filename.split('_')[1]
+                if spk_id not in spks_count.keys():
+                    continue
 
-            spks_count[spk_id] += 1
-            im_id = spk_id+'_'+str(spks_count[spk_id]).zfill(5)+'_'+w_id.zfill(5)
-            im_path = args.out_dir+'/local/images/'+spk_id+'/'+im_id+im_mimetype
+                spks_count[spk_id] += 1
+                im_id = spk_id+'_'+str(spks_count[spk_id]).zfill(5)+'_'+w_id.zfill(5)
+                im_path = join_path(args.out_dir, 'local', 'images', spk_id, im_id+im_mimetype)
 
-            # copy the image
-            copyfile(im_path_orig, im_path)
-
-            # register the sample (randomly split train 95% and test 5%)
-            coin = randint(0, 20)
-            if coin >= 1:
-                text_train.append(im_id+' '+trs[im_filename.rstrip(im_mimetype)]+'\n')  # train/text: <im_id> <transcription>
-                utt2spk_train.append(im_id+' '+spk_id+'\n')                             # train/utt2spk: <im_id> <spk_id>
-                images_train.append(im_id+' '+im_path+'\n')                             # train/images.scp: <im_id> <path_to_image>
-            else:
-                text_test.append(im_id+' '+trs[im_filename.rstrip(im_mimetype)]+'\n')   # test/text: <im_id> <transcription>
-                utt2spk_test.append(im_id+' '+spk_id+'\n')                              # test/utt2spk: <im_id> <spk_id>
-                images_test.append(im_id+' '+im_path+'\n')                              # test/images.scp: <im_id> <path_to_image>
+                # copy the image (and invert colors if wanted)
+                if args.invert:
+                    ImageOps.invert(Image.open(im_orig_path)).save(im_path)
+                else:
+                    copyfile(im_orig_path, im_path)
+                
+                # register the sample (randomly split train 95% and test 5%)
+                coin = randint(0, 20)
+                if coin >= 1:
+                    text_train.append(im_id+' '+trs+'\n')                                   # train/text: <im_id> <transcription>
+                    utt2spk_train.append(im_id+' '+spk_id+'\n')                             # train/utt2spk: <im_id> <spk_id>
+                    images_train.append(im_id+' '+im_path+'\n')                             # train/images.scp: <im_id> <path_to_image>
+                else:
+                    text_test.append(im_id+' '+trs+'\n')                                    # test/text: <im_id> <transcription>
+                    utt2spk_test.append(im_id+' '+spk_id+'\n')                              # test/utt2spk: <im_id> <spk_id>
+                    images_test.append(im_id+' '+im_path+'\n')                              # test/images.scp: <im_id> <path_to_image>
 
     with cod_open(args.out_dir+'/train/text', 'w+', encoding='utf-8') as f:
         for line in sorted(text_train):
